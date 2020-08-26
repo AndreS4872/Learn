f24034652bf79eab6d132236931bb5b41fcbebcb
Andre Siepmann <andresiepmann66@gmail.com>
2020-08-26 13:45:28 +0200

    1

---
+++ Trainer.java
@@ -0,0 +1,12 @@
+public class Trainer extends Person{
+    // Zusätzliche Eigenschaften eines Trainers:
+    private int erfarung;
+    // Funktionen (get und set):
+    public int getErfahrungen(){
+        return erfahrung;
+    }
+            
+    public void setErfahrung(int e){
+        erfahrungen = e;
+    }
+}
